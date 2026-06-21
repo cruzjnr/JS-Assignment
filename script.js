e1a750bfe603cf7ab111bd7635c5c2c92443da1b
Kekemeke John Ifeanyichukwu <ifeanyichukwujohn2005@gmail.com>
2026-06-21 08:46:36 +0100

    Project Completed

---
+++ script.js
@@ -0,0 +1,59 @@
+// Question 1: Variables
+
+let name = "Ifeanyichukwu John";
+let age = 21;
+let school = "Admission status (waiting)";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("School:", school);
+
+// Question 2: Data Types
+
+let studentName = "Bruce";
+let score = 92;
+let isPresent = true;
+
+console.log("Student Name:", studentName, "Type:", typeof studentName);
+console.log("Score:", score, "Type:", typeof score);
+console.log("IS Present:", isPresent, "Type:", typeof isPresent);
+
+
+// Question 3: Operators
+
+let price = 5000;
+let quantity = 5;
+
+console.log("Addition:", price + quantity);
+console.log("Subtraction:", price - quantity);
+console.log("Multiplication:", price * quantity);
+console.log("Division:", price / quantity);
+
+
+// Question 4: Strings and Template Literals
+
+let firstName = "David";
+let lastName = "Peter";
+let age2 = 20;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age2} years old.`);
+
+
+// Question 5: Conditional Statements
+
+let testscore = 75;
+
+if (testscore >= 70) {
+    console.log("Excellent");
+} else if (testscore >= 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+
+// Question 6: Loops
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
